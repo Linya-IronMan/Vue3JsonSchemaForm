b975810e04636c6619ad8778e0c51945019d2fd9
linya <cdthanye32@163.com>
2021-10-24 11:36:33 +0800

    ✨ feat(all): 初始化 基础语法使用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,116 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <div class="hello">{{ state.name }} {{ age }}: {{ age2 }}</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import {
+  defineComponent,
+  PropType,
+  reactive,
+  ref,
+  computed,
+  watchEffect,
+} from "vue"
+
+interface Config {
+  name: string
+}
+
+const PropTypes = {
+  msg: String,
+  config: {
+    type: Object as PropType<Config>,
+    required: true,
+  },
+} as const
 
 export default defineComponent({
   name: "HelloWorld",
-  props: {
-    msg: String,
+  props: PropTypes,
+
+  setup(props, { slots, attrs }) {
+    const state = reactive({
+      name: "cdt",
+    })
+
+    const ageRef = ref(12)
+
+    setInterval(() => {
+      ageRef.value += 1
+    }, 1000)
+
+    const age2 = computed(() => {
+      return ageRef.value + 2
+    })
+
+    watchEffect(() => {
+      console.log(age2.value, ageRef.value, "watchEffect")
+    })
+
+    return {
+      state,
+      age: ageRef,
+      age2,
+    }
   },
-});
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp, defineComponent, h, createVNode } from "vue"
+// import App from "./App.vue"
+const img = require("./assets/logo.png") // eslint-disable-line
 
-createApp(App).mount("#app");
+import HelloWorld from "./components/HelloWorld.vue"
+const App = defineComponent({
+  setup() {
+    return () => {
+      return h("div", { id: "app" }, [
+        h("img", { alt: "Vue log", src: img }),
+        h(HelloWorld as any, { msg: "Hello World", age: 12 }),
+      ])
+    }
+  },
+  //   render() {
+  //     return createVNode("div", { id: "app" }, [
+  //       createVNode("img", { alt: "Vue log", src: img }),
+  //       createVNode(HelloWorld, { msg: "Hello World", age: 12 }),
+  //     ])
+  //   },
+})
+const app = createApp(App).mount("#app")
